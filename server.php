9be28f16bdc3aa898f50229f23ebdc8f825f19e4
koppalc <koppalc1998@gmail.com>
2020-10-20 21:16:02 +0530

    updated Server.php file for storing the file and view the files.

--- server.php
+++ server.php
@@ -18,30 +18,67 @@ if (isset($_POST['enter']))
     $Mobile = $_POST['Mobile'];
     $Hrname   = $_POST['Hrname'];
     $address  = $_POST['address'];
-    $image = $_FILES['image']['name'];
     $password  = $_POST['password'];
     $password_encrypt = md5($password);
-    $target_dir = "upload/";
-    $target_file = $_FILES['image']['name'];
-    //move_uploaded_file($target_file,$target_dir.$image);
-    $subject = "Welcome";
-    $body = "Hi, Thank you for visiting our site";
-$headers = "From: noreply@gmail.com";
 
 
-if (mail($to_email, $subject, $body, $headers)) {
-    echo "Email successfully sent to $to_email...";
-    $query = "INSERT INTO register (Cname, Email, Mobile, Hrname, address, image, password )
-    VALUES ('$name', '$to_email', '$Mobile', '$Hrname','$address','$image','$password_encrypt')";
-    mysqli_query($conn, $query);  
-     header('location: index.php');  
     
-} else {
-    echo "Email sending failed...";
-}
+    $upload_dir = 'uploads'.DIRECTORY_SEPARATOR; 
 
+    # FIle selecting and uploading 
+   if(array_filter($_FILES['files']['name'])) { 
+  
+        // Loop through each file in files[] array 
+        foreach ($_FILES['files']['tmp_name'] as $key => $value) { 
+              
+            $file_tmpname = $_FILES['files']['tmp_name'][$key]; 
+            $file_name = $_FILES['files']['name'][$key]; 
+            $file_size = $_FILES['files']['size'][$key]; 
+            $file_ext = pathinfo($file_name, PATHINFO_EXTENSION); 
 
+            $query = "INSERT INTO register (Cname, Email, Mobile, Hrname, address, image, password )
+            VALUES ('$name', '$to_email', '$Mobile', '$Hrname','$address','$file_name','$password_encrypt')";
+            mysqli_query($conn, $query);  
+            header('location: index.php'); 
+            // Set upload file path 
+            $filepath = $upload_dir.$file_name;   
+  
+                // If file with name already exist then append time in 
+                // front of name of the file to avoid overwriting of file 
+               if(file_exists($filepath)) { 
+                    $filepath = $upload_dir.time().$file_name; 
+                      
+                    if( move_uploaded_file($file_tmpname, $filepath)) { 
+                        echo "{$file_name} successfully uploaded <br />"; 
+                    }  
+                    else {                      
+                        echo "Error uploading {$file_name} <br />";  
+                    } 
+                } 
+                else { 
+                  
+                    if( move_uploaded_file($file_tmpname, $filepath)) { 
+                        echo "{$file_name} successfully uploaded <br />"; 
+                    } 
+                    else {                      
+                        echo "Error uploading {$file_name} <br />";  
+                    } 
+                } 
+            }    
+        }
+         
+    
+    #mail sending ................
+    $subject = "Simple Email Test via PHP";
+    $body = "Hi, This is test email send by PHP Script";
+    $headers = "From: noreply@gmail.com";
 
-}
+     #mail sending cuntion and storing files in DB
+     if (mail($to_email, $subject, $body, $headers)) {
+       echo "Email successfully sent to $to_email...";    
+     } else {
+      echo "Email sending failed...";
+     }
+  }
 
 ?> 
