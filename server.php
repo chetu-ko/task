5a80c29f9f54ad5bf1329e5af881da90fafdd739
koppalc <koppalc1998@gmail.com>
2020-10-20 12:35:02 +0530

    Here Server.php file edited and added the mailsending function to the this file.

--- server.php
+++ server.php
@@ -14,7 +14,7 @@ if (isset($_POST['enter']))
 {
     
     $name   = $_POST['Cname'];
-    $Email  = $_POST['Email'];
+    $to_email  = $_POST['Email'];
     $Mobile = $_POST['Mobile'];
     $Hrname   = $_POST['Hrname'];
     $address  = $_POST['address'];
@@ -22,14 +22,25 @@ if (isset($_POST['enter']))
     $password  = $_POST['password'];
     $password_encrypt = md5($password);
     $target_dir = "upload/";
-    $target_file = $target_dir . basename($_FILES["file"]["image"]);
+    $target_file = $_FILES['image']['name'];
+    //move_uploaded_file($target_file,$target_dir.$image);
+    $subject = "Welcome";
+    $body = "Hi, Thank you for visiting our site";
+$headers = "From: noreply@gmail.com";
 
+
+if (mail($to_email, $subject, $body, $headers)) {
+    echo "Email successfully sent to $to_email...";
     $query = "INSERT INTO register (Cname, Email, Mobile, Hrname, address, image, password )
-    VALUES ('$name', '$Email', '$Mobile', '$Hrname','$address','$image','$password_encrypt')";
-    mysqli_query($conn, $query);
+    VALUES ('$name', '$to_email', '$Mobile', '$Hrname','$address','$image','$password_encrypt')";
+    mysqli_query($conn, $query);  
+     header('location: index.php');  
+    
+} else {
+    echo "Email sending failed...";
+}
 
-    move_uploaded_file($_FILES['file']['tmp_name'],$target_dir.$image);
-    header('location: index.php');
+
 
 }
 
